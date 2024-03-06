3d898edbe6ce836f2125f1b770e3911dff422bdd
Colin Alves <alves.colin@icloud.com>
2024-03-06 15:48:04 +0100

    Setup environment variables and API services

---
+++ src/services/api/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  private apiUrl: string = environment.apiUrl;
+
+  request<T>(method: string, url: string, data?: any) : Observable<T> {
+    let headers: HttpHeaders = new HttpHeaders();
+    return this.http.request<T>(method, this.apiUrl + url, {body: data, headers: headers});
+  }
+}
